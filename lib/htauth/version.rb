6bdae0c8de515520fcc026dc4020ce41b967bbd0
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2015-07-18 18:06:48 -0600

    Simplify version code.

--- lib/htauth/version.rb
+++ lib/htauth/version.rb
@@ -1,21 +1,3 @@
 module HTAuth
   VERSION = "1.2.0"
-  module Version
-    STRING  = HTAuth::VERSION
-    def to_a
-      STRING.split(".")
-    end
-
-    def to_s
-      STRING
-    end
-
-    module_function :to_a
-    module_function :to_s
-
-    MAJOR   = Version.to_a[0]
-    MINOR   = Version.to_a[1]
-    BUILD   = Version.to_a[2]
-
-  end
 end
