ef0722a08c01751c42096d861a4de5ef63a92b1f
copiousfreetime <jeremy@copiousfreetime.org>
2020-04-01 22:30:07 -0600

    remove testing inherited collect as it poisons the child pool

--- spec/algorithm_spec.rb
+++ spec/algorithm_spec.rb
@@ -5,9 +5,4 @@ describe HTAuth::Algorithm do
   it "raises an error if it encouners an unknown algorithm" do
     _ { HTAuth::Algorithm.algorithm_from_name("unknown") }.must_raise(::HTAuth::InvalidAlgorithmError)
   end
-
-  it "raises an error if a child class doesn't implement `handles?`" do
-    klass = Class.new(HTAuth::Algorithm)
-    _ { klass.handles?("foo") }.must_raise(NotImplementedError)
-  end
 end
